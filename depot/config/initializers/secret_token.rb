beb15e1ac8852299f954cd62fdd5882a15af79ec
liyicky <liyicky@gmail.com>
2014-03-02 17:20:42 -0500

    Restarting with a new book: Agile Web Development with Rails 4

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '4aa6cc5936e9850ee70b11ce2304f461d43f1ab10594e8b7e8ff0810452d8278d1f4073cbd606d0f773f17dcd0a2b449cea21c88a939f0e1e48593170bc5a42d'
