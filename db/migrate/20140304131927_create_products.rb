b72ea577c09329f0dd9fd77ce957d05025e09ac3
liyicky <liyicky@gmail.com>
2014-03-04 08:20:43 -0500

    New scaffold

--- db/migrate/20140304131927_create_products.rb
+++ db/migrate/20140304131927_create_products.rb
@@ -1,16 +1,12 @@
 class CreateProducts < ActiveRecord::Migration
-  def self.up
+  def change
     create_table :products do |t|
       t.string :title
       t.text :description
       t.string :image_url
-      t.decimal :price, :precision => 8, :scale => 2
+      t.decimal :price
 
       t.timestamps
     end
-  end
-
-  def self.down
-    drop_table :products
   end
 end
