878f77caeac6a67863fa40a910e6326ff8341d67
liyicky <liyicky@gmail.com>
2014-03-07 09:11:13 -0500

    Product model assertion tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product assertions" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title:        "The Book Title",
+                          description:  "test",
+                          image_url:    "test.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "image_url must be of type .jpg, .gif, or .png" do
+    def new_product(image_url)
+      Product.new(title:        "The Book Title",
+                  description:  "test",
+                  image_url:    image_url)
+    end
+  end
 end
