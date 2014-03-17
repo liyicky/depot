35466c0fd75c796cb2442229266e680227bc7290
liyicky <liyicky@gmail.com>
2014-03-17 09:18:20 -0400

    Testing index html for products

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,15 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+
+    assert_select 'h1', 'Listing Products'
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main', 1
+    assert_select 'dt', 'Cat Penis'
+    assert_select 'dd', 'Cats need a penis. Introducting Cat dicks!'
+    assert_select 'table .list_line_odd', minimum: 1
+    assert_select '.list_actions', minimum: 1
+    assert_select 'a', 'New Product
   end
 
   test "should get new" do
@@ -52,4 +61,6 @@ class ProductsControllerTest < ActionController::TestCase
 
     assert_redirected_to products_path
   end
+
+  test
 end
